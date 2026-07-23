11d3834059ceac315e32c7de28b9fa80490ae5f7
park0372 <krc8372@gmail.com>
2026-07-23 16:10:33 +0300

    Update vendor-manager.js

--- vendor-manager.js
+++ vendor-manager.js
@@ -218,3 +218,52 @@ document.addEventListener(
 
     }
 );
+/* ==========================================
+   Vendor Modal
+========================================== */
+
+const modal = document.getElementById("vendorModal");
+
+const addVendorBtn = document.getElementById("addVendorBtn");
+
+const closeModal = document.getElementById("closeModal");
+
+const saveVendor = document.getElementById("saveVendor");
+
+/* Open */
+
+addVendorBtn.addEventListener("click", () => {
+
+    modal.style.display = "flex";
+
+});
+
+/* Close */
+
+closeModal.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+});
+
+/* Close when clicking outside */
+
+window.addEventListener("click", (e) => {
+
+    if(e.target === modal){
+
+        modal.style.display = "none";
+
+    }
+
+});
+
+/* Save */
+
+saveVendor.addEventListener("click", () => {
+
+    alert("Next Step : Vendor Save Function");
+
+    modal.style.display = "none";
+
+});
